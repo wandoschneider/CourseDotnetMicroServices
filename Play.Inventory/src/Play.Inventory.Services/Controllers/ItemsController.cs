8afd7fa7bcc21873a6d34ecbc3e22743fe084aa3
Wando Schneider <wandoschneider@MacBook-Air-de-Wando.local>
2022-01-26 21:12:01 -0300

    migração para .net 6

---
+++ Play.Inventory/src/Play.Inventory.Services/Controllers/ItemsController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Play.Common;
+using Play.Inventory.Services.Clients;
+using Play.Inventory.Services.Dtos;
+using Play.Inventory.Services.Entities;
+
+namespace Play.Inventory.Services.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IRepository<InventoryItem> inventoryItemsRepository;
+        private readonly IRepository<CatalogItem> catalogItemsRepository;
+
+        public ItemsController(IRepository<InventoryItem> inventoryItemsRepository, IRepository<CatalogItem> catalogItemsRepository)
+        {
+            this.inventoryItemsRepository = inventoryItemsRepository;
+            this.catalogItemsRepository = catalogItemsRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InventoryItemDto>>> GetAsync(Guid userId)
+        {
+            if (userId == Guid.Empty)
+                return BadRequest();
+
+            var inventoryItemEntities = await inventoryItemsRepository.GetAllAsync(item => item.UserId == userId);
+            var itemsId = inventoryItemEntities.Select(item => item.CatalogItemId);
+            var catalogItemEntities = await catalogItemsRepository.GetAllAsync(item => itemsId.Contains(item.Id));
+
+            var inventoryItemDtos = inventoryItemEntities.Select(inventoryItem =>
+            {
+                var catalogItem = catalogItemEntities.Single(catalogItem => catalogItem.Id == inventoryItem.CatalogItemId);
+                return inventoryItem.AsDto(catalogItem.Name, catalogItem.Description);
+            });
+
+            Console.WriteLine($"ItemsController.GetAsync: {inventoryItemDtos.Count()} items found");
+
+            return Ok(inventoryItemDtos);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(GrantItemsDto grantItemsDto)
+        {
+            var inventoryItem = await inventoryItemsRepository.GetAsync(
+                item => item.UserId == grantItemsDto.UserId && item.CatalogItemId == grantItemsDto.CatalogItemId);
+
+            if (inventoryItem is null)
+            {
+                inventoryItem = new InventoryItem
+                {
+                    CatalogItemId = grantItemsDto.CatalogItemId,
+                    UserId = grantItemsDto.UserId,
+                    Quantity = grantItemsDto.Quantity,
+                    AcquiredDate = DateTimeOffset.UtcNow
+                };
+
+                await inventoryItemsRepository.CreateAsync(inventoryItem);
+            }
+            else
+            {
+                inventoryItem.Quantity += grantItemsDto.Quantity;
+                await inventoryItemsRepository.UpdateAsync(inventoryItem);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Play.Inventory/src/Play.Inventory.Services/Startup.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Play.Common.MassTransit;
+using Play.Common.MongoDB;
+using Play.Inventory.Services.Clients;
+using Play.Inventory.Services.Entities;
+using Polly;
+using Polly.Timeout;
+
+namespace Play.Inventory.Services
+{
+    public class Startup
+    {
+        private const string AllowedOriginSetting = "AllowedOrigin";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMongo()
+                    .AddMongoRepository<InventoryItem>("inventoryitems")
+                    .AddMongoRepository<CatalogItem>("catalogitems")
+                    .AddMassTransitWithRabbitMq();
+
+            AddCatalogClient(services);
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Inventory.Services", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Inventory.Services v1"));
+
+                app.UseCors(builder =>
+                {
+                    builder.WithOrigins(Configuration[AllowedOriginSetting])
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static void AddCatalogClient(IServiceCollection services)
+        {
+            Random jitterer = new Random();
+
+            services.AddHttpClient<CatalogClient>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5001");
+            })
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+                5,
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(1, retryAttempt))
+                                + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
+            ))
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                3,
+                TimeSpan.FromSeconds(15)
+            ))
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+        }
+    }
+}
